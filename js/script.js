aa195c6ad0d0508d169aa533c6c5641ebc823fa8
Marcin Świrek <swirek.marcin@gmail.com>
2019-01-19 00:44:09 +0100

    Add navigation section on low screen width

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
-function openTab(event, tabName) {
+// function openTab(event, tabName) {
+//   let i, tabcontent, tablinks;
+//   tabcontent = document.getElementsByClassName('tabcontent');
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = 'none';
+//   }
+//   tablinks = document.getElementsByClassName('tablinks');
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(' active', '');
+//   }
+//   document.getElementById(tabName).style.display = 'block';
+//   event.currentTarget.className += ' active';
+// }
 
-  let i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(tabName).style.display = "block";
-  event.currentTarget.className += " active";
-};
+// openTab();
 
-openTab();
+//Hamburger menu
+
+const hamburger = document.querySelectorAll('.hamburger-icon');
+const activeElements = document.querySelectorAll('.rwd');
+
+hamburger.forEach(icon => icon.addEventListener('click', () => activeElements.forEach(element => element.classList.toggle('active'))))

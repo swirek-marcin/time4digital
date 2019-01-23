bff62017d53deebb791ade1305aa53eaec716a4c
Marcin Świrek <swirek.marcin@gmail.com>
2019-01-23 01:42:52 +0100

    Fix bugs in index.html. Remove commented lines of code

--- js/script.js
+++ js/script.js
@@ -32,33 +32,18 @@ const slideListSmall = [
     img: '../images/slider_big.jpg'
   },
   {
-    img: '../images/hero_2_480.png'
+    img: '../images/hero2.jpg'
   }
 ];
-
-// const slideListMedium = [
-//   {
-//     img: '../images/hero_480.png' //zmiana z 768
-//   },
-//   {
-//     img: '../images/hero_2_480.png'
-//   }
-// ];
 
 const image = document.querySelector('img.slider');
 const h1 = document.querySelector('h1.slider');
 const dots = [...document.querySelectorAll('.dots span')];
 // Interfejs
-const time = 2000;
+const time = 5000;
 let active = 0;
 
 // Implementacje
-
-// const changeDot = () => {
-//   const activeDot = dots.findIndex(dot => dot.classList.contains('active'));
-//   dots[activeDot].classList.remove('active');
-//   dots[active].classList.add('active');
-// };
 
 const changeSlide = () => {
   active++;
@@ -66,8 +51,6 @@ const changeSlide = () => {
     active = 0;
   }
   image.src = slideListSmall[active].img;
-  // h1.textContent = slideList[active].textHeader;
-  // changeDot();
 };
 let indexInterval = setInterval(changeSlide, time);
 
@@ -83,69 +66,9 @@ const keyChangeSlide = e => {
       active = slideListSmall.length - 1;
     }
     image.src = slideListSmall[active].img;
-    // changeDot();
     indexInterval = setInterval(changeSlide, time);
   }
 };
 
 window.addEventListener('keydown', keyChangeSlide);
 
-//Min width 768px;=================================================================
-// const mediaQueryListMedium = window.matchMedia('(min-width: 1024px)');
-
-// if (mediaQueryListMedium.matches) {
-//   const slideListMedium = [
-//     {
-//       img: '../images/slider_big.jpg' //zmiana z 768
-//     },
-//     {
-//       img: '../images/hero_2_480.png'
-//     }
-//   ];
-
-//   const image = document.querySelector('img.slider');
-//   const h1 = document.querySelector('h1.slider');
-//   const dots = [...document.querySelectorAll('.dots span')];
-//   // Interfejs
-//   const time = 2000;
-//   let active = 0;
-
-//   // Implementacje
-
-//   // const changeDot = () => {
-//   //   const activeDot = dots.findIndex(dot => dot.classList.contains('active'));
-//   //   dots[activeDot].classList.remove('active');
-//   //   dots[active].classList.add('active');
-//   // };
-
-//   const changeSlide = () => {
-//     console.log('768');
-//     active++;
-//     if (active === slideListMedium.length) {
-//       active = 0;
-//     }
-//     image.src = slideListMedium[active].img;
-//     // h1.textContent = slideList[active].textHeader;
-//     // changeDot();
-//   };
-//   let indexInterval = setInterval(changeSlide, time);
-
-//   // Klawisze
-//   const keyChangeSlide = e => {
-//     console.log(e.keyCode);
-//     if (e.keyCode == 37 || e.keyCode == 39) {
-//       clearInterval(indexInterval);
-//       e.keyCode == 37 ? active-- : active++;
-//       if (active === slideListMedium.length) {
-//         active = 0;
-//       } else if (active < 0) {
-//         active = slideListMedium.length - 1;
-//       }
-//       image.src = slideListMedium[active].img;
-//       // changeDot();
-//       indexInterval = setInterval(changeSlide, time);
-//     }
-//   };
-
-//   window.addEventListener('keydown', keyChangeSlide);
-// }

44fbbbed928d59662d7dbfa114238fbeff122a71
Marcin Świrek <swirek.marcin@gmail.com>
2019-01-31 13:02:01 +0100

    Add hero btn slider change functionality. Remove focus from btns

--- js/slider.js
+++ js/slider.js
@@ -6,15 +6,17 @@ const slideList = [
   },
   {
     img: '../images/slider_big_2.jpg'
+  },
+  {
+    img: '../images/slider_big_3.png'
   }
 ];
 
 const image = document.querySelector('img.slider');
 const h1 = document.querySelector('h1.slider');
-const dots = [...document.querySelectorAll('.dots span')];
 const leftController = document.querySelector('.slider-controller-left');
 const rightController = document.querySelector('.slider-controller-right');
-
+const sliderControllers = document.querySelectorAll('slider-controller');
 
 // Interfejs
 const time = 5000;
@@ -33,7 +35,7 @@ let indexInterval = setInterval(changeSlide, time);
 
 // Klawisze
 const keyChangeSlide = e => {
-  console.log(e.keyCode);
+  // console.log(e.keyCode);
   if (e.keyCode == 37 || e.keyCode == 39) {
     clearInterval(indexInterval);
     e.keyCode == 37 ? active-- : active++;
@@ -47,4 +49,25 @@ const keyChangeSlide = e => {
   }
 };
 
+//Buttony
+const btnChangeSlide = function(e) {
+  if (e.type == 'click') {
+    console.log(e.target.classList);
+    clearInterval(indexInterval);
+    e.target.classList.contains('fa-angle-right') ||
+    e.target.classList.contains('slider-controller-right')
+      ? active++
+      : active--;
+    if (active === slideList.length) {
+      active = 0;
+    } else if (active < 0) {
+      active = slideList.length - 1;
+    }
+    image.src = slideList[active].img;
+    indexInterval = setInterval(changeSlide, time);
+  }
+};
+
 window.addEventListener('keydown', keyChangeSlide);
+leftController.addEventListener('click', btnChangeSlide);
+rightController.addEventListener('click', btnChangeSlide);

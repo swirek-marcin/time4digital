f84a9664e4caefeb504ebe84babcf64397a8d87d
Marcin Świrek <swirek.marcin@gmail.com>
2019-02-01 12:03:46 +0100

    Rewrite headers. Remove margin from body. Change hamburger menu mechanic

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-// Pseudo routing
-// function openTab(event, tabName) {
-//   let i, tabcontent, tablinks;
-//   tabcontent = document.getElementsByClassName('tabcontent');
-//   for (i = 0; i < tabcontent.length; i++) {
-//     tabcontent[i].style.display = 'none';
-//   }
-//   tablinks = document.getElementsByClassName('tablinks');
-//   for (i = 0; i < tablinks.length; i++) {
-//     tablinks[i].className = tablinks[i].className.replace(' active', '');
-//   }
-//   document.getElementById(tabName).style.display = 'block';
-//   event.currentTarget.className += ' active';
-// }
-
-// openTab();
-
 //Hamburger menu
 
 const hamburger = document.querySelectorAll('.hamburger-icon');
@@ -26,3 +9,8 @@ hamburger.forEach(icon =>
   )
 );
 
+// hamburger.forEach(icon =>
+//   icon.addEventListener('click', () =>
+//     activeElements.forEach(element => console.log('dziala'))
+//   )
+// );
